764b90a9bebaeaad651f4892064c511b18f451cc
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-03-25 15:38:54 -0300

    I created more tests

--- src/sum.js
+++ src/sum.js
@@ -1,7 +1,10 @@
 'use strict';
 
-function sum() {
-  console.log('Teste');
+function sum(n1, n2) {
+  if (!n1 || !n2 && typeof n1 !== 'number' || typeof n2 !== 'number') { 
+    return new Error('Pass two numbers for parameter');
+  }
+  return n1 + n2;
 }
 
 module.exports = sum;
--- test/sum.test.js
+++ test/sum.test.js
@@ -8,7 +8,23 @@ let sum = require('../src/sum');
 
 describe('# SUM', function() {
   // Cada 'it()' será um teste diferente.
-  it('Should SUM module to be a function', function() {
+  it('Should SUM module to be a function', () => {
     expect(sum).to.be.a('function');
+  });
+
+  it('Should SUM return 10 when I pass 1 and 9', () => {
+    expect(sum(1, 9)).to.be.equal(10);
+  });
+
+  it('Should SUM return 5 when I pass 2 and 3', () => {
+    expect(sum(2, 3)).to.be.equal(5);
+  });
+
+  it('Should SUM return an error if it receive just one parameter', () => {
+    expect(sum(2)).to.be.an('error');
+  });
+
+  it('Should SUM return an error if the parameter has not a number', () => {
+    expect(sum('x', 'y')).to.be.an('error');
   });
 });
